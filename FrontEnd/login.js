39e7e158752aa48ef9488ff0831a9bb2050785b8
Laure <laure_ld@outlook.fr>
2024-12-10 19:10:06 +0100

    Ajuster des elements de connection

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -20,34 +20,21 @@ async function login(email, password) {
 }
 }
 
-const emailOk = "sophie.bluel@test.tld"
-const passwordOk ="S0phie"
-
-async function loginOk(){
+async function onLogin (event) {
+    event.preventDefault()
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
-   
-    if(!email && !password){
-        console.error("Veuillez renseigner votre email et mot de passe.");
-        return;
-    }
 
-    if (email === emailOk && password === passwordOk) {
-        try {
-            const response = await login(email, password);
+    try {
+        const response = await login(email, password);
 
-            if(response.token) {
-                window.localStorage.setItem("token", response.token);
-                window.location.href = "connected.html";
-            } else{
-                console.error("Aucun token retourné par l'API.");
+        if(response.token) {
+            window.localStorage.setItem("token", response.token);
+            window.location.href = "connected.html";
+        } else{
+            console.error("error");
             }
-
         } catch(error) {
             console.error("Erreur lors de la connexion :", error);
         }
-    } else{
-        console.error("Email ou mot de passe incorrect.");
-        alert("Email ou mot de passe incorrect.");
     }
-}
