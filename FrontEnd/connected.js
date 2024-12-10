88033cc2ed1dc97cbfd2bbf09cfa3e525d993efd
Laure <laure_ld@outlook.fr>
2024-12-10 19:10:30 +0100

    Créaction des fonction 2eme modal

--- FrontEnd/connected.js
+++ FrontEnd/connected.js
@@ -4,40 +4,44 @@ console.log("Token récupere :", token)
 //access to the modal
 let modal = null 
 
-const openModal = function (event) {
+const openModal = function(event) {
     event.preventDefault()
     modal = document.querySelector(event.target.getAttribute("data-target"));
     modal.style.display = null
     modal.addEventListener("click", closeModal);
     modal.removeAttribute("aria-hidden")
-    modal.querySelector(".close").addEventListener("click", closeModal)
+    modal.querySelectorAll(".close").forEach(btn => {
+        btn.addEventListener("click", closeModal); });
     modal.querySelector(".modal-gallery").addEventListener("click", stopPropagation)
+    modal.querySelector(".modal-add-photo").addEventListener("click", stopPropagation)
     showElementModal()
 }
 
-const closeModal = function (event) {
+const closeModal = function(event) {
     if (modal === null) return
     event.preventDefault()
     modal.style.display = "none"
     modal.removeEventListener("click", closeModal);
     modal.setAttribute("aria-hidden", "true")
-    modal.querySelector(".close").removeEventListener("click", closeModal)
+    modal.querySelectorAll(".close").forEach(btn => {
+        btn.removeEventListener("click", closeModal)});
     modal.querySelector(".modal-gallery").removeEventListener("click", stopPropagation)
+    modal.querySelector(".modal-add-photo").removeEventListener("click", stopPropagation)
     modal = null
 }
 
-const stopPropagation = function (event) {
+const stopPropagation = function(event) {
     event.stopPropagation()
 }
 
-const showElementModal = function () {
+const showElementModal = function() {
     const elementModal = modal.querySelector(".modal-gallery");
     const addProject = modal.querySelector(".modal-add-photo");
     elementModal.style.display = "block";
     addProject.style.display = "none";
-}
+};
 
-const showAddProject = function () {
+const showAddProject = function() {
     const elementModal = modal.querySelector(".modal-gallery");
     const addProject = modal.querySelector(".modal-add-photo");
     elementModal.style.display = "none";
@@ -46,7 +50,7 @@ const showAddProject = function () {
 
 document.querySelectorAll(".modify").forEach(attribut => {
     attribut.addEventListener("click", openModal)
-})
+});
 
 document.querySelector(".more").addEventListener("click", (event) => {
     event.preventDefault();
@@ -56,7 +60,7 @@ document.querySelector(".more").addEventListener("click", (event) => {
 //displaying projects in thumbnail
 const api = "http://localhost:5678/api"
 
-async function getWorks () {
+async function getWorks() {
     try{
         const response = await fetch(api + "/works", {
             method:"GET", 
@@ -76,7 +80,7 @@ async function getWorks () {
     }
 }
 
-function displayInProject (projects2) {
+function displayInProject(projects2) {
     const container = document.querySelector("#portfolio .modal-project-list");
     
     container.innerHTML = "";
@@ -105,15 +109,17 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 });
 
-function btnBin (projects) {
+function btnBin(projects) {
     const container = document.querySelector(".modal-project-list");
+    const mainGallery = document.querySelector(".gallery");
 
-    if (!container) {
+    if (!container || !mainGallery) {
         return;
     }
     container.innerHTML = "";
+    mainGallery.innerHTML = "";
 
-    projects.forEach (project => {
+    projects.forEach(project => {
         const id = project.id
         const imageUrl = project.imageUrl
         const title = project.title
@@ -140,6 +146,9 @@ function btnBin (projects) {
 
         projectElement.appendChild(bin);
         container.appendChild(projectElement);
+
+        const projectMainElement = projectElement.cloneNode(true);
+        mainGallery.appendChild(projectMainElement);
     });
 }
 const autToken = token
@@ -148,12 +157,12 @@ async function deleteProject(id) {
         const responseBin = await fetch (api + `/works/${id}`,{
             method:"DELETE", 
             headers: { 
-                "content-type": "application/json",
                 "Authorization": `Basic ${autToken}`,
+                "content-type": "application/json",
              }   
         });
         
-        if (responseBin.ok && responseBin.status === 204) {
+        if (responseBin.ok && responseBin.status === 200) {
             console.log("Projet supprimé avec succès !");
             return true; 
 
@@ -174,3 +183,65 @@ async function worksProject(){
     }
 }
 worksProject();
+
+//add new project
+const form = document.querySelector(".upload-form");
+
+form.addEventListener("submit", async (event) => {
+    event.preventDefault();
+
+    const formData = new FormData(form);
+    try {
+        const responseBin = await fetch (api + `/works`,{
+            method:"POST", 
+            body: formData,  
+        });
+        
+        if (responseBin.ok && responseBin.status === 201) {
+            console.log("Projet rajouté avec succès !");
+            return true; 
+
+        } else {
+            console.error(`Erreur inattendue : ${responseBin.status}`);
+        }
+        return false;
+
+    } catch (error) {
+        console.error("Erreur lors de l’appel API :", error);
+        return false 
+    }
+});
+
+const categoryChoose = async function(event) {
+    
+    try {
+        const response = await fetch (api + "/categories", {
+            Method : "GET",
+            headers : {
+                "Content-Type": "application/json", },
+        })
+
+        if(!response.ok){
+            console.error("error cat")
+        }
+
+        const categories = await response.json();
+        const categoryDropdown = document.querySelector("#category");
+
+        if (!categoryDropdown) {
+            console.error("Menu déroulant non trouvé !");
+            return;
+        }
+        categoryDropdown.innerHTML = "";
+
+        categories.forEach((category) => {
+            const option = document.createElement("option");
+            option.value = category.id; 
+            option.textContent = category.name; 
+            categoryDropdown.appendChild(option);
+        });
+    }catch (error) {
+        console.error("Erreur lors de la récupération des catégories :", error);
+    }
+};
+document.addEventListener("DOMContentLoaded", categoryChoose);
